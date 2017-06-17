fe73062b6e749ae13b127425b06ef0b459106fae
John Bryan Gaona Trinidad <john.gt81@gmail.com>
2017-06-17 14:23:28 -0500

    implement holidays 2008

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#calculate').click(onCalculateClick);
     $('#startDate').datepicker();
 
-     function onCalculateClick() {
+    function onCalculateClick() {
         var days = $('#days').val();
         var start_date = $('#startDate').val();
         var end_date = moment(start_date, "MM/DD/YYYY").add('days', days);
@@ -11,7 +11,26 @@ $(document).ready(function () {
         if (!isValid(days, start_date, country_code)) return;
 
         clearResults();
-        drawCalendars(start_date, end_date);
+
+        getHolidays(country_code)
+            .done(function(data){
+                drawCalendars(start_date, end_date, data.holidays);
+            })
+            .fail(function(){
+                alert("An error has ocurred calling Holidays API");
+            });
+    }
+
+    function getHolidays(country_code){
+        return $.ajax({
+            method: "GET",
+            url: "https://holidayapi.com/v1/holidays",
+            data: {
+                key: "902627f9-2670-41bb-b4ae-1415ca24eabb",
+                country: country_code,
+                year: "2008"
+            }
+        });
     }
 
     function isValid(days, start_date, country_code){
@@ -30,7 +49,7 @@ $(document).ready(function () {
         $("#results").empty();
     }
 
-    function drawCalendars(start_date, end_date) {
+    function drawCalendars(start_date, end_date, holidays) {
         var startFirstDay = moment(start_date).startOf('month');
         var endFirstDay = moment(end_date).startOf('month');
 
@@ -39,19 +58,21 @@ $(document).ready(function () {
             i++;
             var calendarId = "calendar" + i;
             $("<div id='" + calendarId + "' />").appendTo("#results");
-            $('#' + calendarId).datepicker(setupDatePicker(start_date, end_date, startFirstDay));
+            $('#' + calendarId).datepicker(setupDatePicker(start_date, end_date, startFirstDay, holidays));
             startFirstDay = moment(startFirstDay).add(1, 'months');
         }
     }
 
-    function setupDatePicker(startDate, endDate, defaultDate) {
+    function setupDatePicker(startDate, endDate, defaultDate, holidays) {
         return {
             defaultDate: moment(defaultDate).toDate(),
             hideIfNoPrevNext: true,//not working
             beforeShowDay: function (date) {
                 if (moment(date).isBetween(startDate, endDate, 'day', '[)')) {
-                    if (isHoliday(date))
-                        return [true, "holiday", '']; //orange
+                    if (isHoliday(date, holidays)){
+                        var holiday = holidays[moment(date).format("YYYY-MM-DD")];
+                        return [true, "holiday", holiday[0].name]; //orange
+                    }
                     if (isWeekend(date))
                         return [true, "weekend", '']; //yellow
                     return [true, "weekday", '']; //green olive
@@ -61,8 +82,9 @@ $(document).ready(function () {
         };
     }
 
-     function isHoliday(currentDate) {
-        return false;
+    function isHoliday(currentDate, holidays) {
+        var formattedDate = moment(currentDate).format("YYYY-MM-DD");
+        return holidays[formattedDate] != undefined;
     }
 
     function isWeekend(currentDate) {
